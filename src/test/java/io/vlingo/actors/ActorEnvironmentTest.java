21b141f5c13482aa3cb07a62bf894b8cf645a1d8
Vaughn Vernon <vaughn@forcomprehension.com>
2018-01-02 20:50:21 -0800

    Updated copyright notice to include 2018.

--- src/test/java/io/vlingo/actors/ActorEnvironmentTest.java
+++ src/test/java/io/vlingo/actors/ActorEnvironmentTest.java
@@ -1,4 +1,4 @@
-// Copyright 2012-2017 For Comprehension, Inc.
+// Copyright 2012-2018 For Comprehension, Inc.
 //
 // This Source Code Form is subject to the terms of the
 // Mozilla Public License, v. 2.0. If a copy of the MPL
