581055a527ded4b14c7e9420984a0081fdbace4e
Vaughn Vernon <vaughn@forcomprehension.com>
2018-07-15 20:11:59 -0700

    Properties file loader.

---
+++ src/main/java/io/vlingo/actors/Properties.java
@@ -0,0 +1,24 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.actors;
+
+public class Properties {
+  public static final java.util.Properties properties;
+
+  private static final String propertiesFile = "/vlingo-actors.properties";
+
+  static {
+    properties = new java.util.Properties();
+
+    try {
+      properties.load(Properties.class.getResourceAsStream(propertiesFile));
+    } catch (Exception e) {
+      // fall through
+    }
+  }
+}
