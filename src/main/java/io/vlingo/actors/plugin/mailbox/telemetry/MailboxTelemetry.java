66b57e926064ac6611e33591d3f331f86290c425
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-08-13 23:05:13 +0200

    Implement TelemetryMailbox

--- src/main/java/io/vlingo/actors/plugin/mailbox/telemetry/MailboxTelemetry.java
+++ src/main/java/io/vlingo/actors/plugin/mailbox/telemetry/MailboxTelemetry.java
@@ -1,4 +1,12 @@
 package io.vlingo.actors.plugin.mailbox.telemetry;
 
+import io.vlingo.actors.Message;
+
 public interface MailboxTelemetry {
+  void onSendMessage(final Message message);
+  void onSendMessageFailed(final Message message, final Throwable exception);
+
+  void onPullEmptyMailbox();
+  void onPulledMessage(final Message message);
+  void onPullMessageFailed(final Throwable exception);
 }
---
+++ src/main/java/io/vlingo/actors/plugin/mailbox/telemetry/TelemetryMailbox.java
@@ -0,0 +1,66 @@
+package io.vlingo.actors.plugin.mailbox.telemetry;
+
+import io.vlingo.actors.Mailbox;
+import io.vlingo.actors.Message;
+
+public class TelemetryMailbox implements Mailbox {
+  private final MailboxTelemetry telemetry;
+  private final Mailbox delegate;
+
+  public TelemetryMailbox(final MailboxTelemetry telemetry, final Mailbox delegate) {
+    this.telemetry = telemetry;
+    this.delegate = delegate;
+  }
+
+  @Override
+  public void close() {
+    delegate.close();
+  }
+
+  @Override
+  public boolean isClosed() {
+    return delegate.isClosed();
+  }
+
+  @Override
+  public boolean isDelivering() {
+    return delegate.isDelivering();
+  }
+
+  @Override
+  public boolean delivering(final boolean flag) {
+    return delegate.delivering(flag);
+  }
+
+  @Override
+  public void send(final Message message) {
+    try {
+      delegate.send(message);
+      telemetry.onSendMessage(message);
+    } catch (Exception e) {
+      telemetry.onSendMessageFailed(message, e);
+    }
+  }
+
+  @Override
+  public Message receive() {
+    try {
+      Message receivedMessage = delegate.receive();
+      if (receivedMessage == null) {
+        telemetry.onPullEmptyMailbox();
+      } else {
+        telemetry.onPulledMessage(receivedMessage);
+      }
+
+      return receivedMessage;
+    } catch (RuntimeException e) {
+      telemetry.onPullMessageFailed(e);
+      throw e;
+    }
+  }
+
+  @Override
+  public void run() {
+    delegate.run();
+  }
+}
