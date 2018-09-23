5f7ad927322f703b461069f920352f452ccde6fc
Vaughn Vernon <vaughn@forcomprehension.com>
2018-09-22 23:24:45 -0700

    Refactored to interface.

--- src/main/java/io/vlingo/actors/Address.java
+++ src/main/java/io/vlingo/actors/Address.java
@@ -7,58 +7,12 @@
 
 package io.vlingo.actors;
 
-public final class Address implements Comparable<Address> {
-  final static Address None = new Address(0, "None");
-
-  private final int id;
-  private final String name;
-
-  Address(final int reservedId) {
-    this(reservedId, null);
-  }
-
-  Address(final int reservedId, final String name) {
-    this(reservedId, name, false);
-  }
-
-  Address(final int reservedId, final String name, final boolean prefixName) {
-    this.id = reservedId;
-    this.name = name == null ?
-            Integer.toString(reservedId) :
-            prefixName ? (name + id) : name;
-  }
-
-  public int id() {
-    return id;
-  }
-
-  public String ids() {
-    return ""+id;
-  }
-
-  public final String name() {
-    return name;
-  }
-
-  @Override
-  public boolean equals(final Object other) {
-    if (other == null || other.getClass() != Address.class) {
-      return false;
-    }
-    return id == ((Address) other).id;
-  }
-
-  @Override
-  public int hashCode() {
-    return id;
-  }
-
-  @Override
-  public String toString() {
-    return "Address[" + id + ", name=" + (name == null ? "(none)" : name) + "]";
-  }
-
-  public int compareTo(final Address other) {
-    return Integer.compare(id, other.id);
-  }
+public interface Address extends Comparable<Address> {
+  long id();
+  long idSequence();
+  String idSequenceString();
+  String idString();
+  <T> T idTyped();
+  String name();
+  boolean isDistributable();
 }
