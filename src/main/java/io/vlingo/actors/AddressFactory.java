f0968d9b359fc7921b31b290485ea79194c0029c
Vaughn Vernon <vaughn@forcomprehension.com>
2018-09-22 23:25:22 -0700

    Refactored to interface.

--- src/main/java/io/vlingo/actors/AddressFactory.java
+++ src/main/java/io/vlingo/actors/AddressFactory.java
@@ -7,51 +7,16 @@
 
 package io.vlingo.actors;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-public final class AddressFactory {
-  private final AtomicInteger highId;
-  private final AtomicInteger nextId;
-
-  public Address findableBy(final int id) {
-    return new Address(id, ""+id);
-  }
-
-  public Address from(final int reservedId, final String name) {
-    return new Address(reservedId, name);
-  }
-
-  public int testNextIdValue() {
-    return nextId.get(); // for test only
-  }
-
-  public Address unique() {
-    return new Address(nextId.getAndIncrement());
-  }
-
-  public Address uniquePrefixedWith(final String prefixedWith) {
-    return new Address(nextId.getAndIncrement(), prefixedWith, true);
-  }
-
-  public Address uniqueWith(final String name) {
-    return new Address(nextId.getAndIncrement(), name);
-  }
-
-  public Address withHighId() {
-    return withHighId(null);
-  }
-
-  public Address withHighId(final String name) {
-    return new Address(highId.decrementAndGet(), name);
-  }
-
-  @Override
-  public String toString() {
-    return "AddressFactory[highId=" + highId.get() + ", nextId=" + nextId.get() + "]";
-  }
-
-  AddressFactory() {
-    this.highId = new AtomicInteger(World.HIGH_ROOT_ID);
-    this.nextId = new AtomicInteger(1);
-  }
+public interface AddressFactory {
+  <T> Address findableBy(final T id);
+  Address from(final long reservedId, final String name);
+  Address from(final String idString);
+  Address from(final String idString, final String name);
+  Address none();
+  Address unique();
+  Address uniquePrefixedWith(final String prefixedWith);
+  Address uniqueWith(final String name);
+  Address withHighId();
+  Address withHighId(final String name);
+  long testNextIdValue();
 }
