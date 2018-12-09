dbf14a8566f1dc41bb34d6ea7db4f62a2e799303
Vaughn Vernon <vaughn@forcomprehension.com>
2018-12-09 15:54:48 +0100

    Provided Javadoc.

--- src/main/java/io/vlingo/actors/Actor.java
+++ src/main/java/io/vlingo/actors/Actor.java
@@ -14,31 +14,57 @@ import io.vlingo.common.Completes;
 import io.vlingo.common.ResultCompletes;
 import io.vlingo.common.Scheduler;
 
+/**
+ * The abstract base class of all concrete instance. This base provides common
+ * facilities and life cycle processing for all Actors.
+ */
 public abstract class Actor implements Startable, Stoppable, TestStateView {
   final ResultCompletes completes;
   final LifeCycle lifeCycle;
 
+  /**
+   * Answers the address of this Actor.
+   * @return Address
+   */
   public Address address() {
     return lifeCycle.address();
   }
 
+  /**
+   * Answers the DeadLetters for this Actor.
+   * @return DeadLetters
+   */
   public DeadLetters deadLetters() {
     return lifeCycle.environment.stage.world().deadLetters();
   }
 
+  /**
+   * Answers the Scheduler for this Actor.
+   * @return Scheduler
+   */
   public Scheduler scheduler() {
     return lifeCycle.environment.stage.scheduler();
   }
 
+  /**
+   * The default implementation of start(), which is a no-op. Override if needed.
+   */
   @Override
   public void start() {
   }
 
+  /**
+   * Answers whether or not this Actor has been stopped or is in the process or stopping.
+   * @return boolean
+   */
   @Override
   public boolean isStopped() {
     return lifeCycle.isStopped();
   }
 
+  /**
+   * Initiates the process or stopping this Actor all all of its children.
+   */
   @Override
   public void stop() {
     if (!isStopped()) {
@@ -49,11 +75,20 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     }
   }
 
+  /**
+   * Answers the TestState for this Actor. Override to provide a snapshot of the current Actor state.
+   * @return TestState
+   */
   public TestState viewTestState() {
     // override for concrete actor state
     return new TestState();
   }
 
+  /**
+   * Answers whether or not this Actor is equal to other.
+   * @param other the Object to which this Actor is compared
+   * @return boolean
+   */
   @Override
   public boolean equals(final Object other) {
     if (other == null || other.getClass() != this.getClass()) {
@@ -63,16 +98,28 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     return address().equals(((Actor) other).lifeCycle.address());
   }
 
+  /**
+   * Answers the int hash code of this Actor.
+   * @return int
+   */
   @Override
   public int hashCode() {
     return lifeCycle.hashCode();
   }
 
+  /**
+   * Answers the String representation of this Actor.
+   * @return String
+   */
   @Override
   public String toString() {
     return "Actor[type=" + this.getClass().getSimpleName() + " address=" + address() + "]";
   }
 
+  /**
+   * Answers the parent Actor of this Actor. (INTERNAL ONLY)
+   * @return Actor
+   */
   Actor parent() {
     if (lifeCycle.environment.isSecured()) {
       throw new IllegalStateException("A secured actor cannot provide its parent.");
@@ -80,6 +127,9 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     return lifeCycle.environment.parent;
   }
 
+  /**
+   * Initializes the newly created Actor.
+   */
   protected Actor() {
     final Environment maybeEnvironment = ActorFactory.threadLocalEnvironment.get();
     this.lifeCycle = new LifeCycle(maybeEnvironment != null ? maybeEnvironment : new TestEnvironment());
@@ -87,6 +137,12 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     this.completes = new ResultCompletes();
   }
 
+  /**
+   * Answers the T protocol for the child Actor to be created by this parent Actor.
+   * @param definition the Definition of the child Actor to be created by this parent Actor
+   * @param protocol the Class&lt;T&gt; protocol of the child Actor
+   * @return T
+   */
   protected <T> T childActorFor(final Definition definition, final Class<T> protocol) {
     if (definition.supervisor() != null) {
       return lifeCycle.environment.stage.actorFor(definition, protocol, this, definition.supervisor(), logger());
@@ -99,6 +155,11 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     }
   }
 
+  /**
+   * Answers the Completes&lt;T&gt; instance for this Actor, or null if the behavior of the currently
+   * delivered Message does not answer a Completes&lt;T&gt;.
+   * @return Completes&lt;T&gt;
+   */
   @SuppressWarnings("unchecked")
   protected <T> Completes<T> completes() {
     if (completes == null) {
@@ -107,18 +168,36 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     return (Completes<T>) completes;
   }
 
+  /**
+   * Answers a CompletesEventually if the behavior of the currently
+   * delivered Message does answers a Completes&lt;T&gt;. Otherwise the outcome
+   * is unpredictable.
+   * @return CompletesEventually
+   */
   protected CompletesEventually completesEventually() {
     return lifeCycle.environment.stage.world().completesFor(completes.clientCompletes());
   }
 
+  /**
+   * Answers the Definition of this Actor.
+   * @return Definition
+   */
   protected Definition definition() {
     return lifeCycle.definition();
   }
 
+  /**
+   * Answers the Logger of this Actor.
+   * @return Logger
+   */
   protected Logger logger() {
     return lifeCycle.environment.logger;
   }
 
+  /**
+   * Answers the parent of this Actor as the T protocol.
+   * @return T
+   */
   protected <T> T parentAs(final Class<T> protocol) {
     if (lifeCycle.environment.isSecured()) {
       throw new IllegalStateException("A secured actor cannot provide its parent.");
@@ -127,14 +206,26 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     return lifeCycle.environment.stage.actorProxyFor(protocol, parent, parent.lifeCycle.environment.mailbox);
   }
 
+  /**
+   * Secures this Actor. (INTERNAL ONLY)
+   */
   protected void secure() {
     lifeCycle.secure();
   }
 
+  /**
+   * Answers this Actor as a T protocol. This Actor must implement the Class&lt;T&gt; protocol.
+   * @param protocol the Class&lt;T&gt; protocol
+   * @return T
+   */
   protected <T> T selfAs(final Class<T> protocol) {
     return lifeCycle.environment.stage.actorProxyFor(protocol, this, lifeCycle.environment.mailbox);
   }
 
+  /**
+   * Answers the Stage of this Actor.
+   * @return Stage
+   */
   protected final Stage stage() {
     if (lifeCycle.environment.isSecured()) {
       throw new IllegalStateException("A secured actor cannot provide its stage.");
@@ -142,6 +233,11 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
     return lifeCycle.environment.stage;
   }
 
+  /**
+   * Answers the Stage of the given name.
+   * @param name the String name of the Stage to find
+   * @return Stage
+   */
   protected Stage stageNamed(final String name) {
     return lifeCycle.environment.stage.world().stageNamed(name);
   }
@@ -150,18 +246,34 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
   // stowing/dispersing
   //=======================================
 
+  /**
+   * Answers whether this Actor is currently dispersing previously stowed messages.
+   * @return boolean
+   */
   protected boolean isDispersing() {
     return lifeCycle.isDispersing();
   }
 
+  /**
+   * Starts the process of dispersing any messages stowed for this Actor.
+   */
   protected void disperseStowedMessages() {
     lifeCycle.disperseStowedMessages();
   }
 
+  /**
+   * Answers whether this Actor is currently stowing messages.
+   * @return boolean
+   */
   protected boolean isStowing() {
     return lifeCycle.isStowing();
   }
 
+  /**
+   * Starts the process of stowing messages for this Actor, and registers stowageOverrides as
+   * the protocol that will trigger dispersal.
+   * @param stowageOverrides the Class&lt;T&gt; protocol that will trigger dispersal
+   */
   protected void stowMessages(final Class<?>... stowageOverrides) {
     lifeCycle.stowMessages();
     lifeCycle.environment.stowageOverrides(stowageOverrides);
@@ -171,24 +283,45 @@ public abstract class Actor implements Startable, Stoppable, TestStateView {
   // life cycle overrides
   //=======================================
 
+  /**
+   * The message delivered before the Actor has fully started. Override to implement.
+   */
   protected void beforeStart() {
     // override
   }
 
+  /**
+   * The message delivered after the Actor has fully stopped. Override to implement.
+   */
   protected void afterStop() {
     // override
   }
 
+  /**
+   * The message delivered before the Actor has been restarted by its supervisor due to an exception.
+   * Override to implement.
+   * @param reason the Throwable cause of the supervision restart
+   */
   protected void beforeRestart(final Throwable reason) {
     // override
     lifeCycle.afterStop(this);
   }
 
+  /**
+   * The message delivered after the Actor has been restarted by its supervisor due to an exception.
+   * Override to implement.
+   * @param reason the Throwable cause of the supervision restart
+   */
   protected void afterRestart(final Throwable reason) {
     // override
     lifeCycle.beforeStart(this);
   }
 
+  /**
+   * The message delivered before the Actor has been resumed by its supervisor due to an exception.
+   * Override to implement.
+   * @param reason the Throwable cause of the supervision resume
+   */
   protected void beforeResume(final Throwable reason) {
     // override
   }
