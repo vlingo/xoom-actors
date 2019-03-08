6d60a95a591cf613c937f1a4e13a5b06f3e9839c
VaughnVernon <vaughn@forcomprehension.com>
2019-03-08 10:37:27 -0700

    Defined maybeActorOf(). Added Javadoc.

--- src/main/java/io/vlingo/actors/DirectoryScanner.java
+++ src/main/java/io/vlingo/actors/DirectoryScanner.java
@@ -7,8 +7,32 @@
 
 package io.vlingo.actors;
 
+import java.util.Optional;
+
 import io.vlingo.common.Completes;
 
+/**
+ * Protocol to find an actor of a given address and protocol.
+ */
 public interface DirectoryScanner {
-  public <T> Completes<T> actorOf(final Class<T> protocol, final Address address);
+  /**
+   * Answer the {@code protocol} reference of the actor with {@code address} as a
+   * {@code Completes<T>} eventual outcome, or {@code null} as a {@code Completes<T>}
+   * eventual outcome if not found.
+   * @param protocol the {@code Class<T>} of the protocol that the actor must support
+   * @param address the {@code Address} of the actor to find
+   * @param <T> the protocol type
+   * @return {@code Completes<T>}
+   */
+  <T> Completes<T> actorOf(final Class<T> protocol, final Address address);
+
+  /**
+   * Answer the {@code protocol} reference of the actor with {@code address} as a non-empty
+   * {@code Completes<Optional<T>>} eventual outcome, or an empty {@code Optional} if not found.
+   * @param protocol the {@code Class<T>} of the protocol that the actor must support
+   * @param address the {@code Address} of the actor to find
+   * @param <T> the protocol type
+   * @return {@code Completes<Optional<T>>}
+   */
+  <T> Completes<Optional<T>> maybeActorOf(final Class<T> protocol, final Address address);
 }
