a4b5fd1c9ee8c5706055bf7c4ad32d3e3e5bbfb4
VaughnVernon <vaughn@forcomprehension.com>
2019-03-08 10:37:57 -0700

    Implemented maybeActorOf().

--- src/main/java/io/vlingo/actors/DirectoryScannerActor.java
+++ src/main/java/io/vlingo/actors/DirectoryScannerActor.java
@@ -7,6 +7,8 @@
 
 package io.vlingo.actors;
 
+import java.util.Optional;
+
 import io.vlingo.common.Completes;
 
 public class DirectoryScannerActor extends Actor implements DirectoryScanner {
@@ -16,19 +18,43 @@ public class DirectoryScannerActor extends Actor implements DirectoryScanner {
     this.directory = directory;
   }
 
+  /*
+   * @see io.vlingo.actors.DirectoryScanner#actorOf(java.lang.Class, io.vlingo.actors.Address)
+   */
   @Override
   public <T> Completes<T> actorOf(final Class<T> protocol, final Address address) {
+    return completes().with(internalActorOf(protocol, address));
+  }
+
+  /*
+   * @see io.vlingo.actors.DirectoryScanner#maybeActorOf(java.lang.Class, io.vlingo.actors.Address)
+   */
+  @Override
+  public <T> Completes<Optional<T>> maybeActorOf(Class<T> protocol, Address address) {
+    final T typed = internalActorOf(protocol, address);
+    final Optional<T> maybe = typed == null ? Optional.empty() : Optional.of(typed);
+    return completes().with(maybe);
+  }
+
+  /**
+   * Answer the actor as the {@code protocol} or {@code null}.
+   * @param protocol the {@code Class<T>} of the protocol that the actor must support
+   * @param address the {@code Address} of the actor to find
+   * @param T the protocol type
+   * @return T
+   */
+  private <T> T internalActorOf(final Class<T> protocol, final Address address) {
     final Actor actor = directory.actorOf(address);
 
     try {
       if (actor != null) {
-        return completes().with(stage().actorAs(actor, protocol));
+        return stage().actorAs(actor, protocol);
       } else {
         logger().log("Actor with address: " + address + " not found; protocol is: " + protocol.getName());
       }
     } catch (Exception e) {
       logger().log("Error providing protocol: " + protocol.getName() + " for actor with address: " + address, e);
     }
-    return completes().with(null);
+    return null;
   }
 }
