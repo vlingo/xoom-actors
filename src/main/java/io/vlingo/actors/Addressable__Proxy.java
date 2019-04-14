6113385ce6eb2e2a4d2d420f0cbbf49d877b7c3d
VaughnVernon <vaughn@forcomprehension.com>
2019-04-14 10:25:53 -0700

    Initial revision of internal-only protocol proxy.

---
+++ src/main/java/io/vlingo/actors/Addressable__Proxy.java
@@ -0,0 +1,26 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.actors;
+
+public class Addressable__Proxy implements Addressable {
+  private final Actor actor;
+
+  public Addressable__Proxy(final Actor actor, final Mailbox mailbox) {
+    this.actor = actor;
+  }
+
+  @Override
+  public Address address() {
+    return actor.address();
+  }
+
+  @Override
+  public LifeCycle lifeCycle() {
+    return actor.lifeCycle;
+  }
+}
