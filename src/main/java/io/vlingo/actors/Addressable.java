e38ccd8b8f2eeaa6ad44d4d464ab95670ba0f926
VaughnVernon <vaughn@forcomprehension.com>
2019-04-14 10:25:35 -0700

    Initial revision of internal-only protocol.

---
+++ src/main/java/io/vlingo/actors/Addressable.java
@@ -0,0 +1,18 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.actors;
+
+/**
+ * The means for an {@code Actor} to provide its {@code Address}.
+ *
+ * WARNING: For internal use only.
+ */
+public interface Addressable {
+  Address address();
+  LifeCycle lifeCycle();
+}
