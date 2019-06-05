8309a749bc6ba3b691e7036400443e232c3cf6d6
SillentTroll <guzun.alex.c@gmail.com>
2019-06-05 21:33:55 +0100

    Rewrite the test using AccessSafely

--- src/test/java/io/vlingo/actors/LocalMessageTest.java
+++ src/test/java/io/vlingo/actors/LocalMessageTest.java
@@ -14,95 +14,96 @@ import java.util.function.Consumer;
 
 import org.junit.Test;
 
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
+import io.vlingo.actors.testkit.TestActor;
 
 public class LocalMessageTest extends ActorsTest {
   @Test
-  public void testDeliverHappy() throws Exception {
-    final SimpleTestResults testResults = new SimpleTestResults();
-    
-    testWorld.actorFor(Simple.class, Definition.has(SimpleActor.class, Definition.parameters(testResults), "test1-actor"));
-    
+  public void testDeliverHappy() {
+    final SimpleTestResults testResults = new SimpleTestResults(1);
+
+    final TestActor<Simple> testActor = testWorld.actorFor(Simple.class,
+            Definition.has(SimpleActor.class, Definition.parameters(testResults), "test1-actor"));
+
     final Consumer<Simple> consumer = (actor) -> actor.simple();
-    final LocalMessage<Simple> message = new LocalMessage<Simple>(SimpleActor.instance.get(), Simple.class, consumer, "simple()");
-    
-    testResults.untilSimple = TestUntil.happenings(1);
-    
+    final LocalMessage<Simple> message = new LocalMessage<Simple>(testActor.actorInside(), Simple.class, consumer, "simple()");
+
     message.deliver();
-    
-    testResults.untilSimple.completes();
-    
-    assertEquals(1, testResults.deliveries.get());
+
+    assertEquals(1, testResults.getDeliveries());
   }
 
   @Test
-  public void testDeliverStopped() throws Exception {
-    final SimpleTestResults testResults = new SimpleTestResults();
-    
-    testWorld.actorFor(Simple.class, Definition.has(SimpleActor.class, Definition.parameters(testResults), "test2-actor"));
-    
-    testResults.untilSimple = TestUntil.happenings(1);
-    
-    SimpleActor.instance.get().stop();
+  public void testDeliverStopped() {
+    final SimpleTestResults testResults = new SimpleTestResults(0);
+
+    final TestActor<Simple> testActor = testWorld.actorFor(Simple.class,
+            Definition.has(SimpleActor.class, Definition.parameters(testResults), "test2-actor"));
+
+    testActor.actorInside().stop();
         
-    final Consumer<Simple> consumer = (actor) -> actor.simple();
-    final LocalMessage<Simple> message = new LocalMessage<Simple>(SimpleActor.instance.get(), Simple.class, consumer, "simple()");
+    final Consumer<Simple> consumer = actor -> actor.simple();
+    final LocalMessage<Simple> message = new LocalMessage<Simple>(testActor.actorInside(), Simple.class, consumer, "simple()");
     
     message.deliver();
-    
-    assertEquals(1, testResults.untilSimple.remaining());
-    
-    assertEquals(0, testResults.deliveries.get());
+
+    assertEquals(0, testResults.getDeliveries());
   }
 
   @Test
-  public void testDeliverWithParameters() throws Exception {
-    final SimpleTestResults testResults = new SimpleTestResults();
-    
-    testWorld.actorFor(Simple.class, Definition.has(SimpleActor.class, Definition.parameters(testResults), "test3-actor"));
-    
-    testResults.untilSimple = TestUntil.happenings(1);
-    
+  public void testDeliverWithParameters() {
+    final SimpleTestResults testResults = new SimpleTestResults(1);
+
+    final TestActor<Simple> testActor = testWorld.actorFor(Simple.class,
+            Definition.has(SimpleActor.class, Definition.parameters(testResults), "test3-actor"));
+
     final Consumer<Simple> consumer = (actor) -> actor.simple2(2);
-    final LocalMessage<Simple> message = new LocalMessage<Simple>(SimpleActor.instance.get(), Simple.class, consumer, "simple2(int)");
+    final LocalMessage<Simple> message = new LocalMessage<Simple>(testActor.actorInside(), Simple.class, consumer, "simple2(int)");
     
     message.deliver();
-    
-    testResults.untilSimple.completes();
-    
-    assertEquals(1, testResults.deliveries.get());
+
+    assertEquals(1, testResults.getDeliveries());
   }
   
-  public static interface Simple {
+  public static interface Simple extends Stoppable{
     void simple();
     void simple2(final int val);
   }
 
   public static class SimpleActor extends Actor implements Simple {
-    public static final ThreadLocal<SimpleActor> instance = new ThreadLocal<>();
-    
     private final SimpleTestResults testResults;
     
     public SimpleActor(final SimpleTestResults testResults) {
       this.testResults = testResults;
-      instance.set(this);
     }
 
     @Override
     public void simple() {
-      testResults.deliveries.incrementAndGet();
-      testResults.untilSimple.happened();
+      testResults.increment();
     }
 
     @Override
     public void simple2(final int val) {
-      testResults.deliveries.incrementAndGet();
-      testResults.untilSimple.happened();
+      testResults.increment();
     }
   }
   
-  public static class SimpleTestResults {
-    public AtomicInteger deliveries = new AtomicInteger(0);
-    public TestUntil untilSimple = TestUntil.happenings(0);
+  private static class SimpleTestResults {
+    private AccessSafely deliveries;
+
+    private SimpleTestResults(final int times) {
+      final AtomicInteger deliveries = new AtomicInteger(0);
+      this.deliveries = AccessSafely.afterCompleting(times);
+      this.deliveries.writingWith("deliveries", (Integer i)-> deliveries.incrementAndGet());
+      this.deliveries.readingWith("deliveries", deliveries::get);
+    }
+
+    private void increment(){
+      this.deliveries.writeUsing("deliveries", 1);
+    }
+
+    private int getDeliveries(){
+      return this.deliveries.<Integer>readFrom("deliveries");
+    }
   }
 }
