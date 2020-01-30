e6e1c764101cf27fff9b316a2d5faae822f97d25
Alexandros Bourantas <buritos@gmail.com>
2020-01-30 23:54:32 +0100

    moves SerializableConsumer

--- src/main/java/io/vlingo/common/SerializableConsumer.java
+++
@@ -1,8 +0,0 @@
-package io.vlingo.common;
-
-import java.io.Serializable;
-import java.util.function.Consumer;
-
-@FunctionalInterface
-public interface SerializableConsumer<T> extends Consumer<T>, Serializable {
-}
