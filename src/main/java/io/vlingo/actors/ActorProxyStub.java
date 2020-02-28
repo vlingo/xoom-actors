4fc1f969e0ce5936d5fd4b5e4390db2be3023b19
Alexandros Bourantas <buritos@gmail.com>
2020-02-28 10:39:40 +0100

    adds serializable stub for sending actor references over the wire (WIP)

---
+++ src/main/java/io/vlingo/actors/ActorProxyStub.java
@@ -0,0 +1,12 @@
+package io.vlingo.actors;
+
+class ActorProxyStub<T> extends ActorProxyBase<T> {
+
+  private static final long serialVersionUID = -7711746801630270471L;
+
+  public ActorProxyStub(Actor actor) {
+    //super(null, actor);
+  }
+
+  public ActorProxyStub() { }
+}
