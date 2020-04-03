3f30a6ae2b51e642b96029bdd125ce75e5d56297
Petru Flueras <petru.flueras@gmail.com>
2020-04-03 23:29:36 +0300

    Trigger downstream builds of projects

---
+++ .travis/trigger_deps.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Params:
+#   - Downstream module name (e.g. vlingo-actors)
+#   - Release version (e.g. 1.0.4)
+#   - Next development version (e.g. 1.0.5-SNAPSHOT)
+trigger_dependency()
+{
+    echo "Triggering $1 v$2. Next development version $3"
+
+    cd $TRAVIS_BUILD_DIR/..
+    git clone --depth=50 --branch=master https://${GITHUB_TOKEN}@github.com/vlingo/$1.git
+    cd $1
+
+    # Update project version and all io.vlingo dependencies
+    mvn versions:set -DnewVersion=$VERSION
+    mvn versions:use-dep-version -Dincludes=io.vlingo -DdepVersion=$VERSION -DforceVersion=true
+
+    git add pom.xml
+    git commit -m "Release v$2"
+    git tag "$2" -m "Release v$2"
+
+    # Prepare for next development version
+    mvn versions:set -DnewVersion=$3
+    mvn versions:use-dep-version -Dincludes=io.vlingo -DdepVersion=$3 -DforceVersion=true
+    git add pom.xml
+    git commit -m "Next development version $3"
+
+    git push --follow-tags
+}
+
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Detected version $VERSION"
+
+git config --global user.email "VaughnVernon"
+git config --global user.name "vaughn@forcomprehension.com"
+
+# New version
+MAJOR=$(echo $VERSION | cut -f 1 -d '.')
+MINOR=$(echo $VERSION | cut -f 2 -d '.')
+PATCH=$(echo $VERSION | cut -f 3 -d '.')
+NEW_VERSION=$MAJOR.$MINOR.$(($PATCH + 1))-SNAPSHOT
+
+for dependency in "vlingo-wire" "vlingo-telemetry" "vlingo-streams";
+do
+    trigger_dependency $dependency $VERSION $NEW_VERSION
+done
