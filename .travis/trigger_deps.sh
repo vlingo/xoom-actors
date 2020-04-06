668bf17feb9a6cbd34f79b0e5830f4dee68ec112
Petru Flueras <petru.flueras@gmail.com>
2020-04-06 19:19:43 +0300

    Changed the order of projects for release

--- .travis/trigger_deps.sh
+++ .travis/trigger_deps.sh
@@ -41,7 +41,7 @@ MINOR=$(echo $VERSION | cut -f 2 -d '.')
 PATCH=$(echo $VERSION | cut -f 3 -d '.')
 NEW_VERSION=$MAJOR.$MINOR.$(($PATCH + 1))-SNAPSHOT
 
-for dependency in "vlingo-wire" "vlingo-telemetry" "vlingo-streams";
+for dependency in "vlingo-wire" "vlingo-telemetry";
 do
     trigger_dependency $dependency $VERSION $NEW_VERSION
 done
