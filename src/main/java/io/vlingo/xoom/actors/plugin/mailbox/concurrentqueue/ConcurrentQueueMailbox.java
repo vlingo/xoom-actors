847f4adcdde385afae0b3b9ef7b66c28b4b91d54
VaughnVernon <vaughn@kalele.io>
2022-06-04 08:50:31 -0700

    Moved SuspendedDeliveryOverrides outside the ConcurrentQueueMailbox for reuse.
    Signed-off-by: Brian Wehrle <brian.w@miro.com>

--- src/main/java/io/vlingo/xoom/actors/plugin/mailbox/concurrentqueue/ConcurrentQueueMailbox.java
+++ src/main/java/io/vlingo/xoom/actors/plugin/mailbox/concurrentqueue/ConcurrentQueueMailbox.java
@@ -7,19 +7,17 @@
 
 package io.vlingo.xoom.actors.plugin.mailbox.concurrentqueue;
 
-import io.vlingo.xoom.actors.Dispatcher;
-import io.vlingo.xoom.actors.Mailbox;
-import io.vlingo.xoom.actors.Message;
-import io.vlingo.xoom.actors.ResumingMailbox;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
+
+import io.vlingo.xoom.actors.Dispatcher;
+import io.vlingo.xoom.actors.Mailbox;
+import io.vlingo.xoom.actors.Message;
+import io.vlingo.xoom.actors.ResumingMailbox;
+import io.vlingo.xoom.actors.plugin.mailbox.SuspendedDeliveryOverrides;
+import io.vlingo.xoom.actors.plugin.mailbox.SuspendedDeliveryOverrides.Overrides;
 
 public class ConcurrentQueueMailbox implements Mailbox, Runnable {
   private AtomicBoolean delivering;
@@ -81,8 +79,7 @@ public class ConcurrentQueueMailbox implements Mailbox, Runnable {
 
   @Override
   public boolean isSuspendedFor(String name) {
-    return !suspendedDeliveryOverrides.get()
-        .find(name).isEmpty();
+    return !suspendedDeliveryOverrides.get().find(name).isEmpty();
   }
 
   @Override
@@ -123,142 +120,11 @@ public class ConcurrentQueueMailbox implements Mailbox, Runnable {
     return queue.size();
   }
 
-  protected ConcurrentQueueMailbox(final Dispatcher dispatcher, final int throttlingCount) {
+  ConcurrentQueueMailbox(final Dispatcher dispatcher, final int throttlingCount) {
     this.dispatcher = dispatcher;
     this.delivering = new AtomicBoolean(false);
     this.suspendedDeliveryOverrides = new AtomicReference<>(new SuspendedDeliveryOverrides());
     this.queue = new ConcurrentLinkedQueue<Message>();
     this.throttlingCount = (byte) throttlingCount;
-  }
-
-  private static class SuspendedDeliveryOverrides {
-    private final AtomicBoolean accessible;
-    private final List<Overrides> overrides;
-
-    SuspendedDeliveryOverrides() {
-      this.accessible = new AtomicBoolean(false);
-      this.overrides = new ArrayList<>(0);
-    }
-
-    boolean isEmpty() {
-      return overrides.isEmpty();
-    }
-
-    boolean matchesTop(final Class<?> messageType) {
-      final Overrides overrides = peek();
-      if (overrides != null) {
-        for (final Class<?> type : overrides.types) {
-          if (messageType == type) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-
-    Overrides peek() {
-      int retries = 0;
-      while (true) {
-        if (accessible.compareAndSet(false, true)) {
-          Overrides temp = null;
-          if (!isEmpty()) {
-            temp = overrides.get(0);
-          }
-          accessible.set(false);
-          return temp;
-        } else {
-          if (++retries > 100_000_000) {
-            (new Exception()).printStackTrace();
-            return null;
-          }
-        }
-      }
-    }
-
-    List<Overrides> find(final String name) {
-      int retries = 0;
-      while (true) {
-        if (accessible.compareAndSet(false, true)) {
-          List<Overrides> overridesNamed = this.overrides.stream()
-              .filter(o -> o.name.equals(name))
-              .collect(Collectors.toCollection(ArrayList::new));
-
-          accessible.set(false);
-          return overridesNamed;
-        } else {
-          if (++retries > 100_000_000) {
-            (new Exception()).printStackTrace();
-            return Collections.emptyList();
-          }
-        }
-      }
-    }
-
-    boolean pop(final String name) {
-      boolean popped = false;
-      int retries = 0;
-      while (true) {
-        if (accessible.compareAndSet(false, true)) {
-          int elements = overrides.size();
-          for (int index = 0; index < elements; ++index) {
-            if (name.equals(overrides.get(index).name)) {
-              if (index == 0) {
-                overrides.remove(index);
-                popped = true;
-                --elements;
-                while (index < elements) {
-                  if (overrides.get(index).obsolete) {
-                    overrides.remove(index);
-                    --elements;
-                  } else {
-                    break;
-                  }
-                }
-              } else {
-                overrides.get(index).obsolete = true;
-              }
-              accessible.set(false);
-              break;
-            }
-          }
-          break;
-        } else {
-          if (++retries > 100_000_000) {
-            (new Exception()).printStackTrace();
-            return false;
-          }
-        }
-
-      }
-      return popped;
-    }
-
-    void push(final Overrides overrides) {
-      int retries = 0;
-      while (true) {
-        if (accessible.compareAndSet(false, true)) {
-          this.overrides.add(overrides);
-          accessible.set(false);
-          break;
-        } else {
-          if (++retries > 100_000_000) {
-            (new Exception()).printStackTrace();
-            return;
-          }
-        }
-      }
-    }
-  }
-
-  private static class Overrides {
-    final String name;
-    boolean obsolete;
-    final Class<?>[] types;
-
-    Overrides(final String name, final Class<?>[] types) {
-      this.name = name;
-      this.types = types;
-      this.obsolete = false;
-    }
   }
 }
