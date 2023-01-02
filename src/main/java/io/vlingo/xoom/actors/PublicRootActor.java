349c766561a09afceba53dcde226523a9c911a31
Jakub Zalas <jakub@zalas.pl>
2023-01-02 20:39:37 +0000

    Update the copyright notice

--- src/main/java/io/vlingo/xoom/actors/PublicRootActor.java
+++ src/main/java/io/vlingo/xoom/actors/PublicRootActor.java
@@ -1,4 +1,4 @@
-// Copyright © 2012-2022 VLINGO LABS. All rights reserved.
+// Copyright © 2012-2023 VLINGO LABS. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the
 // Mozilla Public License, v. 2.0. If a copy of the MPL
